a2063be7b62b1a0bf3fd59c3c62b2113c08e661e
ning669 <67736398+ning669@users.noreply.github.com>
2024-08-05 16:11:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 #test
 print ("hello world!")
-#test on web
+#test on webx = [...] ## replace this with your object
+try:
+    result = x.index("c")
+except AttributeError:
+    if isinstance(x, list):
+        result = "The list does not contain the element 'c'."
+    else:
+     result = (f"The {type(x).__name__} object does not have the .index() method.")
+
+except ValueError:
+    if isinstance(x, list):
+        result = "The list does not contain the element 'c'."
+    else:
+        result = f"The {type(x).__name__} object does not have the .index() method."
+
+print(result)
